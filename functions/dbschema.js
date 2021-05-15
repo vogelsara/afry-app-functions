4f1e7653744b9f0e2bfbcaa6c1816ef07f8c9823
sara_vogel <sara.vogel@libitum.com>
2021-05-15 23:32:31 +0200

    Add dbschema

---
+++ functions/dbschema.js
@@ -0,0 +1,15 @@
+let db = {
+    people: [
+        {
+            name: 'John Doe',
+            companyId: 'cmpdEbj9Ssax8vGSr9el',
+            createdAt: '2021-05-11T19:16:41.123Z'
+        }
+    ],
+    companies: [
+        {
+            name: 'Comapany Name',
+            createdAt: '2021-05-11T19:16:41.123Z'
+        }
+    ]
+}
