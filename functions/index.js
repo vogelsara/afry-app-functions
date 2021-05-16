94757b8ce4302922408f81af65f31ab19030030e
sara_vogel <sara.vogel@libitum.com>
2021-05-16 20:58:38 +0200

    Add first test

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,46 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDeIAjlMfkAbCoxYKr8wFCtpaEBjAZLfa0",
+    authDomain: "afry-test.firebaseapp.com",
+    projectId: "afry-test",
+    storageBucket: "afry-test.appspot.com",
+    messagingSenderId: "237583337120",
+    appId: "1:237583337120:web:0e5f88d374d7f3035c6bc5",
+    measurementId: "G-WTRG32SNEP"
+  };
 
+admin.initializeApp();
+const db = admin.firestore();
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+app.get('/people', (req, res) => {
+    db.collection('people')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+            let people = [];
+            data.forEach(doc => {
+                people.push({
+                    personId: doc.id,
+                    name: doc.data().name,
+                    companyId: doc.data().companyId,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return res.json(people);
+        })
+        .catch(err => console.error(err));
+});
+
+exports.api = functions.region('europe-west1').https.onRequest(app); 
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,9 +1,30 @@
 axios = require('axios');
-axios.defaults.baseURL = "http://localhost:5001/afry-test-xxx/europe-west1/api";
+axios.defaults.baseURL = "http://localhost:5001/afry-test/europe-west1/api";
 
-describe('#find()', function() {
+const { expect } = require('chai');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('people api', function() {
   it('responds with matching records', async function() {
-    const people = await axios.get('/people');
-    users.should.have.length(3);
+    const expectedResult = [
+      {
+        name: 'Kalle Kula',
+        companyId: null,
+        createdAt: '2021-05-11T19:17:41.123Z'
+      },
+      {
+        name: 'John Doe',
+        companyId: 'cmpdEbj9Ssax8vGSr9el',
+        createdAt: '2021-05-11T19:16:41.123Z'
+      }
+    ]
+    const response = await axios.get('/people');
+    const people = response.data;
+    const peopleWithoutId = people.map(person => {
+      delete person.personId;
+      return person;
+    });
+    expect(peopleWithoutId).to.eql(expectedResult);
   });
-});
+}); 
